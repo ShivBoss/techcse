8541c909aa23cfa9ddbb6c22fdc55397efc25bdb
Shiv Keshri <shivprakashkeshri@gmail.com>
2017-09-10 15:18:59 +0530

    commit settings

--- TechQ/settings.py
+++ TechQ/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['techcse.herokuapp.com/']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['techcse.herokuapp.com']
 
 
 # Application definition
